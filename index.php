c506409155a1705c3134bbfba5ae9c19eb280fa5
Ginaaa07 <97644724+Ginaaa07@users.noreply.github.com>
2022-03-19 16:13:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portofolio | Gina</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary shadow">
+        <div class="container">
+            <a class="navbar-brand text-white" href="#">Gina</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Project</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>    
+
+    <section class="jumbotron text-center">
+        <img src="assets/img/foto.jpg" alt="" class="img-thumbnail rounded-circle">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </section>
+
+</body>
+</html>
